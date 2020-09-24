17da0539ce27be6c69861f925119d1a752d7924e
XefiCodes <71811790+XefiCodes@users.noreply.github.com>
2020-09-24 10:50:12 +0400

    Add files via upload

---
+++ Exercise 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int firstNumber = 1;
+    bool iCanCode = true;
+    char hopefulGrade = 'a';
+	float myDecimal = 1.0;
+	string minimalSentance = "y";
+	double keyMash = 13213123;
+	float mysteryDataType = 5.6f; //Use Google to work this out
+}
+
